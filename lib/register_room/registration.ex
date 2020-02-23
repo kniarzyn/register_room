1158223de1878c845f3d393ea6ee2607fc147d73
pawel.kwietniewski <kniarzyn@gmail.com>
2020-02-23 13:52:15 +0100

    Add UTC_layer
    Registration context jest warstwą, która dba o to, żeby dat trafiające
    do aplikacji były lokalnymi datami, a daty zapisywane do DB były
    datami UTC

--- lib/register_room/registration.ex
+++ lib/register_room/registration.ex
@@ -6,23 +6,61 @@ defmodule RegisterRoom.Registration do
 
   def list_vouchers do
     Repo.all(Voucher)
+    |> Enum.map(&voucher_dates_to_local(&1))
   end
 
-  def get_voucher!(id), do: Repo.get!(Voucher, id)
+  def get_voucher!(id) do
+    Repo.get!(Voucher, id)
+    |> voucher_dates_to_local()
+  end
 
   def create_voucher(attrs \\ %{}) do
     %Voucher{}
     |> Voucher.changeset(attrs)
+    |> voucher_dates_to_utc()
     |> Repo.insert()
   end
 
   def update_voucher(%Voucher{} = voucher, attrs) do
     voucher
     |> Voucher.changeset(attrs)
+    |> voucher_dates_to_utc()
     |> Repo.update()
   end
 
   def change_voucher(%Voucher{} = voucher) do
     Voucher.changeset(voucher, %{})
+  end
+
+  defp voucher_dates_to_local(%Voucher{} = voucher) do
+    Map.put(voucher, :due_date, utc_to_local(voucher.due_date))
+  end
+
+  defp voucher_dates_to_local(%Ecto.Changeset{} = voucher) do
+    {_, utc_datetime} = Ecto.Changeset.fetch_field(voucher, :due_date)
+    local_datetime = utc_to_local(utc_datetime)
+
+    voucher
+    |> Ecto.Changeset.change(due_date: local_datetime)
+  end
+
+  defp voucher_dates_to_utc(voucher_changeset) do
+    {_, local_datetime} = Ecto.Changeset.fetch_field(voucher_changeset, :due_date)
+    utc_datetime = local_to_utc(local_datetime)
+
+    voucher_changeset
+    |> Ecto.Changeset.change(due_date: utc_datetime)
+  end
+
+  defp local_to_utc(local_datetime) do
+    {_, utc_datetime} = local_datetime |> DateTime.shift_zone("UTC")
+
+    utc_datetime
+  end
+
+  defp utc_to_local(utc_datetime) do
+    {_, local_datetime} = utc_datetime |> DateTime.shift_zone("Europe/Warsaw")
+
+    local_datetime
   end
 end
